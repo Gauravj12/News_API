c60861445e7b5c34b8c9ea25febfc8102cc50678
Gauravj12 <gauravjadhav1205@gmail.com>
2023-04-25 21:12:18 +0530

    second commit

--- src/Components/NewsItems.jsx
+++ src/Components/NewsItems.jsx
@@ -10,9 +10,9 @@ export default class NewsItems extends Component {
         <div className="card" style={{width: "18rem"}}>
   <img src={imgUrl} className="card-img-top" alt="..."/>
   <div className="card-body">
-    <h5 className="card-title">{title}</h5>
-    <p className="card-text">{description}</p>
-    <a href="/" className="btn btn-primary btn-sm">Go somewhere</a>
+    <h5 className="card-title">{title}...</h5>
+    <p className="card-text">{description}...</p>
+    <a href={newsUrl} target='_blank' rel="noopener noreferrer" className="btn btn-primary btn-sm">Go somewhere</a>
   </div>
 </div>
       </div>
