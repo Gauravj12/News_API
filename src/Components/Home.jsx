6f30e235a608625cb0ce097b99fa15c06d305a09
Gauravj12 <gauravjadhav1205@gmail.com>
2023-05-23 10:56:09 +0530

    router updation

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
-
+import Navbar from './Navbar'
 
 
 const Home=()=> {
   return (
-    <div className='container-fluid'>
+    <div>
+    <Navbar/>
+    <div className='container-fluid' style={{marginTop:'70px'}}>
     <div className="card text-center">
   <div className="card-body">
     <h5 className="card-title">News API</h5>
     <p className="card-text">News API is a simple, easy-to-use REST API that returns JSON search results for current and historic news articles published by over 80,000 worldwide sources.</p>
-    <a href="https://newsapi.org/" rel="noreferrer" target='_blank' className="btn btn-primary">Go to site..</a>
+    <a href="https://newsapi.org/" rel="noreferrer" target='_blank' className="btn btn-primary">newsapi.org</a>
   </div>
 </div>
-</div>
+</div></div>
   )
 }
 
